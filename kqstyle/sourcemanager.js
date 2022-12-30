f2ebf06431040ee91bfcc304d2ffaf8997004726
ktsuttlemyre <ktsuttlemyre@users.noreply.github.com>
2022-12-29 20:46:36 -0500

    Update sourcemanager.js

--- kqstyle/sourcemanager.js
+++ kqstyle/sourcemanager.js
@@ -180,7 +180,7 @@ appendTo('head',inject('link',{href:"https://cdn.jsdelivr.net/npm/bootstrap@5.3.
 appendTo(document.body,inject('script',{src:"https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js", integrity:"sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN", crossorigin:"anonymous"},function(){
 	  // remote script has loaded
       }));
-ajax("https://ktsuttlemyre.github.io/KillerCommentator/templates/video_source_modal.tmpl",function(data){
+ajax("./templates/video_source_modal.tmpl",function(data){
 	data=data.split(/\r?\n---/);
 	let modalSRC = data[0]
 	let itemSRC = data[1]
