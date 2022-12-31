75f4fdf79922222af6eb12295e09bdd93e107cb2
ktsuttlemyre <ktsuttlemyre@users.noreply.github.com>
2022-12-31 15:58:18 -0500

    Update killer-commentator.js

--- killer-commentator.js
+++ killer-commentator.js
@@ -1,7 +1,7 @@
 include=function(){function g(){var a=this.readyState;if(!a||/ded|te/.test(a))b--,!b&&e&&f()}var a=arguments,c=document,b=a.length,f=a[b-1],e=f.call;e&&b--;for(var d=0;d<b;d++)a=c.createElement("script"),a.src=arguments[d],a.async=!0,a.onload=a.onerror=a.onreadystatechange=g,(c.head||c.getElementsByTagName("head")[0]).appendChild(a)};
 include("https://ktsuttlemyre.github.io/KillerCommentator/plugin_platform.js",function(){
+	//platform plguin ready to use
 	include("https://cdn.jsdelivr.net/npm/interactjs@1.10.17/dist/interact.min.js","svgscribble.js","/kqstyle/sourcemanager.js","speechcommands.js",function(){
-		
 		//add logo and activate
 		ajax("logo/index.html",function(html){
 			appendTo(document.body,domParse(html));
@@ -15,10 +15,11 @@ include("https://ktsuttlemyre.github.io/KillerCommentator/plugin_platform.js",fu
 			SVGScribble.toggle();
 		});
 	})
-});
 
-//inject fontawesome
-//appendTo(document.body,inject('script',{src:"https://kit.fontawesome.com/48764efa36.js", crossorigin:"anonymous"},function(){}));
-appendTo('head',inject('link',{href:"	https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css", rel:"stylesheet", type:"text/css", crossorigin:"anonymous"}))
-//inject logo
-appendTo('head',inject('link',{href:"logo/index.css", rel:"stylesheet", type:"text/css", crossorigin:"anonymous"}))  
+	//inject fontawesome
+	//appendTo(document.body,inject('script',{src:"https://kit.fontawesome.com/48764efa36.js", crossorigin:"anonymous"},function(){}));
+	appendTo('head',inject('link',{href:"	https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css", rel:"stylesheet", type:"text/css", crossorigin:"anonymous"}))
+	//inject logo
+	appendTo('head',inject('link',{href:"logo/index.css", rel:"stylesheet", type:"text/css", crossorigin:"anonymous"})) 
+});
+ 
