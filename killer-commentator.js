0a5406f354bf1bc6656d278d09373299b43ad010
ktsuttlemyre <ktsuttlemyre@users.noreply.github.com>
2022-12-31 16:36:30 -0500

    Update killer-commentator.js

--- killer-commentator.js
+++ killer-commentator.js
@@ -8,80 +8,17 @@ include("https://ktsuttlemyre.github.io/KillerCommentator/plugin_platform.js",fu
 		base_site+"kqstyle/sourcemanager.js",
 		base_site+"speechcommands.js",function(){
 		//add logo and activate
-		ajax(base_site+"logo/index.html",function(html){
-			let logo=domParse(html);
-			prependTo(document.body,logo);
-			//add speech commands 
-			interact('.killer-commentator')
-			  .on('tap', function (event) {
-			    event.preventDefault()
-			    event.currentTarget.classList.add('listening')
-			    voice.start()
-			  })
-			  .on('doubletap', function (event) {
-			    event.preventDefault()
-			    event.currentTarget.classList.toggle('deleting')
-			    setTimeout(function(){event.currentTarget.classList.remove('deleting')},10000)
-			  })
-			  .on('hold', function (event) {
-// 			    event.currentTarget.classList.toggle('rotate')
-// 			    event.currentTarget.classList.remove('large')
-			  })
-			  .draggable({
-			    // enable inertial throwing
-			    inertia: true,
-			    // keep the element within the area of it's parent
-			    modifiers: [
-			      interact.modifiers.restrictRect({
-				restriction: 'parent',
-				endOnly: true
-			      })
-			    ],
-			    // enable autoScroll
-			    autoScroll: true,
-
-			    listeners: {
-			      // call this function on every dragmove event
-			      move: dragMoveListener,
-
-			      // call this function on every dragend event
-			      end (event) {
-
-			      }
-			    }
-			  })
-
-			function dragMoveListener (event) {
-			  var target = event.target
-			  // keep the dragged position in the data-x/data-y attributes
-			  var x = (parseFloat(target.getAttribute('data-x')) || 0) + event.dx
-			  var y = (parseFloat(target.getAttribute('data-y')) || 0) + event.dy
-
-			  // translate the element
-			  target.style.transform = 'translate(' + x + 'px, ' + y + 'px)'
-
-			  // update the posiion attributes
-			  target.setAttribute('data-x', x)
-			  target.setAttribute('data-y', y)
-			}
-
-			// this function is used later in the resizing and gesture demos
-			window.dragMoveListener = dragMoveListener
-
-			
-			
-			
-			
-			
-			
-		});
-		
 		//add scribble toolbar
 		ajax(base_site+"toolbar.html",function(html){
 			appendTo(document.body,domParse(html))
-			SVGScribble.init();
-			SVGScribble.toggle();
+			startScribble()
+			ajax(base_site+"logo/index.html",function(html){
+				let logo=domParse(html);
+				prependTo(document.body,logo);
+				main()
+			});
 		});
+
 	})
 
 	//inject fontawesome
@@ -91,3 +28,66 @@ include("https://ktsuttlemyre.github.io/KillerCommentator/plugin_platform.js",fu
 	appendTo('head',inject('link',{href:base_site+"logo/index.css", rel:"stylesheet", type:"text/css", crossorigin:"anonymous"})) 
 });
  
+startScribble=function(){
+	SVGScribble.init();
+	SVGScribble.toggle();
+}
+
+main = function(){
+	//add speech commands 
+	interact('.killer-commentator')
+	  .on('tap', function (event) {
+	    event.preventDefault()
+	    event.currentTarget.classList.add('listening')
+	    voice.start()
+	  })
+	  .on('doubletap', function (event) {
+	    event.preventDefault()
+	    event.currentTarget.classList.toggle('deleting')
+	    setTimeout(function(){event.currentTarget.classList.remove('deleting')},10000)
+	  })
+	  .on('hold', function (event) {
+// 			    event.currentTarget.classList.toggle('rotate')
+// 			    event.currentTarget.classList.remove('large')
+	  })
+	  .draggable({
+	    // enable inertial throwing
+	    inertia: true,
+	    // keep the element within the area of it's parent
+	    modifiers: [
+	      interact.modifiers.restrictRect({
+		restriction: 'parent',
+		endOnly: true
+	      })
+	    ],
+	    // enable autoScroll
+	    autoScroll: true,
+
+	    listeners: {
+	      // call this function on every dragmove event
+	      move: dragMoveListener,
+
+	      // call this function on every dragend event
+	      end (event) {
+
+	      }
+	    }
+	  })
+
+	function dragMoveListener (event) {
+	  var target = event.target
+	  // keep the dragged position in the data-x/data-y attributes
+	  var x = (parseFloat(target.getAttribute('data-x')) || 0) + event.dx
+	  var y = (parseFloat(target.getAttribute('data-y')) || 0) + event.dy
+
+	  // translate the element
+	  target.style.transform = 'translate(' + x + 'px, ' + y + 'px)'
+
+	  // update the posiion attributes
+	  target.setAttribute('data-x', x)
+	  target.setAttribute('data-y', y)
+	}
+
+	// this function is used later in the resizing and gesture demos
+	window.dragMoveListener = dragMoveListener
+}
