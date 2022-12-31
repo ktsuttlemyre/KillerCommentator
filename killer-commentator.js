ed25513196cc277a6c2602378694aa5f9bf70b75
ktsuttlemyre <ktsuttlemyre@users.noreply.github.com>
2022-12-31 16:01:42 -0500

    Update killer-commentator.js

--- killer-commentator.js
+++ killer-commentator.js
@@ -1,7 +1,8 @@
+var base_site="https://ktsuttlemyre.github.io/KillerCommentator/"
 include=function(){function g(){var a=this.readyState;if(!a||/ded|te/.test(a))b--,!b&&e&&f()}var a=arguments,c=document,b=a.length,f=a[b-1],e=f.call;e&&b--;for(var d=0;d<b;d++)a=c.createElement("script"),a.src=arguments[d],a.async=!0,a.onload=a.onerror=a.onreadystatechange=g,(c.head||c.getElementsByTagName("head")[0]).appendChild(a)};
 include("https://ktsuttlemyre.github.io/KillerCommentator/plugin_platform.js",function(){
 	//platform plguin ready to use
-	include("https://cdn.jsdelivr.net/npm/interactjs@1.10.17/dist/interact.min.js","svgscribble.js","/kqstyle/sourcemanager.js","speechcommands.js",function(){
+	include("https://cdn.jsdelivr.net/npm/interactjs@1.10.17/dist/interact.min.js",base_site+"svgscribble.js",base_site+"kqstyle/sourcemanager.js",base_site+"speechcommands.js",function(){
 		//add logo and activate
 		ajax("logo/index.html",function(html){
 			appendTo(document.body,domParse(html));
