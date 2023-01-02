45092f3e4d4035a7e30beccb3b6d3137464be3bc
ktsuttlemyre <ktsuttlemyre@users.noreply.github.com>
2023-01-02 17:47:07 -0500

    Update sourcemanager.js

--- kqstyle/sourcemanager.js
+++ kqstyle/sourcemanager.js
@@ -7,13 +7,11 @@ let config={
 
 appendTo('head',inject('link',{href:base_site+"kqstyle/sourcemanager.css", rel:"stylesheet", type:"text/css", crossorigin:"anonymous"})) 
 
-
-generateId= function() {
+generateId=function() {
 	return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
 		var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
 		return v.toString(16);
-};
-
+})};
 
 SourceManager={}
 SourceManager.sources={
