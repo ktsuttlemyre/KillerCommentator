8f38bcc67af4f3896e9bc0040976e46613ba14ec
ktsuttlemyre <ktsuttlemyre@users.noreply.github.com>
2023-01-02 15:55:38 -0500

    Update sourcemanager.js

--- kqstyle/sourcemanager.js
+++ kqstyle/sourcemanager.js
@@ -174,12 +174,12 @@ SourceManager.discoverComponents=function(){
 		    listeners: {
 		      move (event) {
 			var target = event.target
-			var x = (parseFloat(target.getAttribute('data-x')) || 0)
-			var y = (parseFloat(target.getAttribute('data-y')) || 0)
+			var x = (Math.floor(parseFloat(target.getAttribute('data-x'))) || 0)
+			var y = (Math.floor(parseFloat(target.getAttribute('data-y'))) || 0)
 
 			// update the element's style
-			target.style.width = event.rect.width + 'px'
-			target.style.height = event.rect.height + 'px'
+			target.style.width = Math.floor(event.rect.width) + 'px'
+			target.style.height = Math.foor(event.rect.height) + 'px'
 
 			// translate when resizing from top or left edges
 			x += event.deltaRect.left
