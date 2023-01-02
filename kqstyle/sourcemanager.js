9dc44c1b0cb163a90b2936a514f68852fabd2680
ktsuttlemyre <ktsuttlemyre@users.noreply.github.com>
2023-01-02 18:16:22 -0500

    Update sourcemanager.js

--- kqstyle/sourcemanager.js
+++ kqstyle/sourcemanager.js
@@ -130,7 +130,7 @@ SourceManager.discoverComponents=function(){
 	let createDOMSizer=function(container,child,width,height){
 		let aspectRatio = width/height;
 		function snapVideoToContainer(x,y,w,h){
-			console.log('resizing video')
+			console.log('resizing video');
 			(w!=null) && child.style.width=`${w}px`
 			(h!=null) && child.style.height=`${h}px`
 			(x!=null) && child.style.transform = `translate(${x}px, ${y}px)`
