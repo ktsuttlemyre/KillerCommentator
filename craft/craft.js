d620e9ef593f7978356a8f169d2ad2909c3c576b
ktsuttlemyre <ktsuttlemyre@users.noreply.github.com>
2023-01-06 14:03:15 -0500

    Update craft.js

--- craft/craft.js
+++ craft/craft.js
@@ -1,21 +1,27 @@
 
-let ensureOverlap=function(elem1,elem2){
+let gappingOnSide=function(elem1,elem2){
   let rect1=elem1.getBoundingClientRect()
   let rect2=elem2.getBoundingClientRect()
-  let notCollide=''
+  let notCovering=''
+  //let dems={}
+  
   if(rect1.left < rect2.left){
-    notCollide+='right'
+    notCovering+='left'
+    //dems.x=rect1.left
   }
-  if(rect1.left > rect2.left){
-    notCollide+='left'
+  if(rect1.right > rect2.right){
+    notCovering+='right'
   } 
   if(rect1.bottom > rect2.bottom){
-    notCollide+='bottom'
+    notCovering+='bottom'
   }
   if(rect1.top < rect2.top){
-    notCollide+='top'
+    notCovering+='top'
+    //dems.y=rec1.dataset.y
   }
+  
   console.log('need to fix=',notCollide)
+  return notCovering
 }
 
 let craft = function(target){
@@ -24,19 +30,26 @@ let craft = function(target){
   let debounceId;
   let resetDebounce=5000
   let editMode=false
-  let currentAngle=0
-  let currentScale=1
-  let currentWidth=0
-  let currentHeight=0
+
   let initGestDist=0
   let mediaElem=target.querySelector('video,img')
+  let targetPos={}
+  let mediaPos={x:0,y:0,width:0,height:0,angle:0,scale:0}
+                        //x:0,
+                        //y:0,
+                        //width:mediaElem.style.width||mediaElem.videoWidth,
+                        //height:mediaElem.style.height||mediaElem.videoHeight
+                       //}
+  
   
   
   let startFn=function(event) {
     clearTimeout(debounceId)
   }
   let endFn=function(event) {
-    ensureOverlap(target,mediaElem)
+    //TODO future optimization. Only check for collision at the end of the move
+    // then calculate difference to move the elemet to proper position
+    //gappingOnSide(target,mediaElem)
     
     debounceId = setTimeout(endEditMode, resetDebounce)
   }
@@ -49,22 +62,23 @@ let craft = function(target){
     editMode=true
     target.classList.add('edit-mode')
   }
-  let dragMoveFn=function (target,dx,dy) {
-          if(!editMode){
+  let dragMoveFn=function (target,dx,dy,force) {
+          if(!force){
+            if(!editMode){
             return
-          }
-    
-    // keep the dragged position in the data-x/data-y attributes
-    var x = (parseFloat(target.getAttribute('data-x')) || 0) + dx
-    var y = (parseFloat(target.getAttribute('data-y')) || 0) + dy
+          }}
 
-    // translate the element
-    target.style.transform = 'translate(' + x + 'px, ' + y + 'px)'
+          // keep the dragged position in the data-x/data-y attributes
+          var x = (parseFloat(target.getAttribute('data-x')) || 0) + dx
+          var y = (parseFloat(target.getAttribute('data-y')) || 0) + dy
 
-    // update the posiion attributes
-    target.setAttribute('data-x', x)
-    target.setAttribute('data-y', y)
-  }
+          // translate the element
+          target.style.transform = 'translate(' + x + 'px, ' + y + 'px)'
+
+          // update the posiion attributes
+          target.setAttribute('data-x', x)
+          target.setAttribute('data-y', y)
+        }
   interact(target).pointerEvents({
   holdDuration: 5000,
   })
@@ -81,13 +95,14 @@ let craft = function(target){
           var x = (parseFloat(target.getAttribute('data-x')) || 0)
           var y = (parseFloat(target.getAttribute('data-y')) || 0)
 
-          // update the element's style
-          target.style.width = event.rect.width + 'px'
-          target.style.height = event.rect.height + 'px'
-
           // translate when resizing from top or left edges
           x += event.deltaRect.left
           y += event.deltaRect.top
+          
+          
+          // update the element's style
+          target.style.width = event.rect.width + 'px'
+          target.style.height = event.rect.height + 'px'
 
           target.style.transform = 'translate(' + x + 'px,' + y + 'px)'
 
@@ -141,19 +156,20 @@ let craft = function(target){
     })  .gesturable({
       listeners: {
         start:function(event){
-          currentWidth=parseFloat(mediaElem.style.width)||mediaElem.videoWidth||0
-          currentHeight=parseFloat(mediaElem.style.height)||mediaElem.videoHeight||0
+          mediaPos.width=parseFloat(mediaElem.style.width)||mediaElem.videoWidth||0
+          mediaPos.height=parseFloat(mediaElem.style.height)||mediaElem.videoHeight||0
           
-          if((isNaN(currentWidth)||
-             currentWidth==''||
-             currentWidth==null||
-             currentWidth<=0)
+          if((isNaN(mediaPos.width)||
+             mediaPos.width==''||
+             mediaPos.width==null||
+             mediaPos.width<=0)
             &&
-             (isNaN(currentHeight)||
-             currentHeight==''||
-             currentHeight==null||
-             parseFloat(currentHeight<=0))){
-             currentWidth=300
+             (isNaN(mediaPos.height)||
+             mediaPos.height==''||
+             mediaPos.height==null||
+             parseFloat(mediaPos.height<=0))){
+                //technically i should wait for video.loadeddata or video.loadedmetadata event and read video.videoWidth
+                mediaPos.width=300
              }
           
           initGestDist=event.distance
@@ -166,18 +182,28 @@ let craft = function(target){
           //let vector = event.scale*currentScale
           //currentScale=vector
           let initGestDelta=event.distance-initGestDist
-          console.log(currentWidth,initGestDist-event.distance,style.height)
+          console.log(mediaPos.width,initGestDist-event.distance,style.height)
           //let vector = (Math.abs(event.dy)>Math.abs(event.dx))?event.dy:event.dx;
           if(style.height=="auto"||style.height==''||style.height==null||parseFloat(style.height<=0)){
-            style.width=currentWidth+initGestDelta+'px'
+            style.width=mediaPos.width+initGestDelta+'px'
+            if(gappingOnSide(target,mediaElem)){
+              style.width=mediaPos.width+'px'
+             }
           }else{
-            style.height=currentHeight+initGestDelta+'px'
+            style.height=mediaPos.height+initGestDelta+'px'
+            if(gappingOnSide(target,mediaElem)){
+              style.width=mediaPos.height+'px'
+             }
           }
+
           dragMoveFn(mediaElem,event.dx,event.dy)
+          if(gappingOnSide(target,mediaElem)){
+            dragMoveFn(mediaElem,event.dx*-1,event.dy*-1)
+          }
         },
         end:function(){
-          currentWidth=parseFloat(target.style.width)||0
-          currentHeight=parseFloat(target.style.height)||0
+          mediaPos.width=parseFloat(target.style.width)||0
+          mediaPos.height=parseFloat(target.style.height)||0
           endFn()
         }
       }
