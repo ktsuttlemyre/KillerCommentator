4dfe4801aa4086d3f5ce4021e160c2ded0e4c103
ktsuttlemyre <ktsuttlemyre@users.noreply.github.com>
2023-01-06 17:52:29 -0500

    Update craft.js

--- craft/craft.js
+++ craft/craft.js
@@ -154,11 +154,11 @@ let craft = function(target){
     }
   startEditMode()
     }).gesturable({
-        modifiers: [
-        interact.modifiers.restrictEdges({
-          inner: mediaElem
-        }),
-        ],
+//         modifiers: [
+//         interact.modifiers.restrictEdges({
+//           inner: mediaElem
+//         }),
+//         ],
       listeners: {
         start:function(event){
           mediaPos.width=parseFloat(mediaElem.style.width)||mediaElem.videoWidth||0
@@ -191,20 +191,20 @@ let craft = function(target){
           //let vector = (Math.abs(event.dy)>Math.abs(event.dx))?event.dy:event.dx;
           if(style.height=="auto"||style.height==''||style.height==null||parseFloat(style.height<=0)){
             style.width=mediaPos.width+initGestDelta+'px'
-//             if(gappingOnSide(target,mediaElem)){
-//               style.width=mediaPos.width+'px'
-//              }
+             if(gappingOnSide(target,mediaElem)){
+               style.width=mediaPos.width+'px'
+              }
           }else{
             style.height=mediaPos.height+initGestDelta+'px'
-//             if(gappingOnSide(target,mediaElem)){
-//               style.width=mediaPos.height+'px'
-//              }
+             if(gappingOnSide(target,mediaElem)){
+               style.width=mediaPos.height+'px'
+              }
           }
 
           dragMoveFn(mediaElem,event.dx,event.dy)
-//           if(gappingOnSide(target,mediaElem)){
-//             dragMoveFn(mediaElem,event.dx*-1,event.dy*-1)
-//           }
+           if(gappingOnSide(target,mediaElem)){
+             dragMoveFn(mediaElem,event.dx*-1,event.dy*-1)
+           }
         },
         end:function(){
           mediaPos.width=parseFloat(mediaElem.style.width)||0
