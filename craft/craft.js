96a1b9ee329918242b2a684a3eedb2fb973ecf95
ktsuttlemyre <ktsuttlemyre@users.noreply.github.com>
2023-01-07 20:32:55 -0500

    Update craft.js

--- craft/craft.js
+++ craft/craft.js
@@ -172,56 +172,56 @@ let craft = function(target,options){
         inertia: false
       })
 
-        .resizable({
-        edges: { left: handles.ml,
-                right: handles.mr,
-                bottom: handles.mb,
-                top: handles.mt
-	       },
+//         .resizable({
+//         edges: { left: handles.ml,
+//                 right: handles.mr,
+//                 bottom: handles.mb,
+//                 top: handles.mt
+// 	       },
 
-        listeners: {
-          start:startFn,
-          move (event) {
-            if(!editMode){
-              return
-            }
-            var x = (parseFloat(target.getAttribute('data-x')) || 0)
-            var y = (parseFloat(target.getAttribute('data-y')) || 0)
+//         listeners: {
+//           start:startFn,
+//           move (event) {
+//             if(!editMode){
+//               return
+//             }
+//             var x = (parseFloat(target.getAttribute('data-x')) || 0)
+//             var y = (parseFloat(target.getAttribute('data-y')) || 0)
 
-            // translate when resizing from top or left edges
-            x += event.deltaRect.left
-            y += event.deltaRect.top
+//             // translate when resizing from top or left edges
+//             x += event.deltaRect.left
+//             y += event.deltaRect.top
 
 
-            // update the element's style
-            target.style.width = event.rect.width + 'px'
-            target.style.height = event.rect.height + 'px'
+//             // update the element's style
+//             target.style.width = event.rect.width + 'px'
+//             target.style.height = event.rect.height + 'px'
 
-            target.style.transform = 'translate(' + x + 'px,' + y + 'px)'
+//             target.style.transform = 'translate(' + x + 'px,' + y + 'px)'
 
-            target.setAttribute('data-x', x)
-            target.setAttribute('data-y', y)
+//             target.setAttribute('data-x', x)
+//             target.setAttribute('data-y', y)
 
-          },end:endFn
-        },
-        modifiers: [
-          // keep the edges inside the parent
-          interact.modifiers.restrictEdges({
-            outer: mediaElem
-          }),
+//           },end:endFn
+//         },
+//         modifiers: [
+//           // keep the edges inside the parent
+//           interact.modifiers.restrictEdges({
+//             outer: mediaElem
+//           }),
 
-          // minimum size
-          interact.modifiers.restrictSize({
-            min: { width: 100, height: 50 },
-           // max: mediaElem
-          }),
-	  interact.modifiers.restrictEdges({
-            outer: 'parent',
-          })
+//           // minimum size
+//           interact.modifiers.restrictSize({
+//             min: { width: 100, height: 50 },
+//            // max: mediaElem
+//           }),
+// 	  interact.modifiers.restrictEdges({
+//             outer: 'parent',
+//           })
 		
-        ],
-        inertia: false
-      })
+//         ],
+//         inertia: false
+//       })
 
       .draggable({
         listeners: {
