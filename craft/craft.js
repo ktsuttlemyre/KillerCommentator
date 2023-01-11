3ccadb5b9f421d735b5071608ba0492ea2901a1c
ktsuttlemyre <ktsuttlemyre@users.noreply.github.com>
2023-01-10 22:16:02 -0500

    Update craft.js

--- craft/craft.js
+++ craft/craft.js
@@ -123,6 +123,7 @@ let craftZone = function(id, geometry) {
 			if(isOver){
 				return getCenter()
 			}
+			return {}
 		},
 		getCenter:getCenter,
 		isSecondary:zone.id.indexOf('secondary') >= 0,
