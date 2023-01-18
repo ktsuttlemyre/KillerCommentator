ea0f18e2d94d0086ab0ca12f254936b029c9a4ac
ktsuttlemyre <ktsuttlemyre@users.noreply.github.com>
2023-01-18 00:29:01 -0500

    Update craft.js

--- craft/craft.js
+++ craft/craft.js
@@ -190,7 +190,7 @@ let craft = function(target, options) {
 	let id=target.id
 
 	let initGestDist = 0
-	let dropSnapRange = 100
+	let dropSnapRange = 150
 	let editDebounceId = null;
 	let resetDebounce = 5000
 	let resetDebounceCustom
