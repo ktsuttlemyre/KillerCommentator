22e8b424ed15d0033f8381d508ec8b76bcac1124
ktsuttlemyre <ktsuttlemyre@users.noreply.github.com>
2023-01-19 21:41:43 -0500

    Update craft.js

--- craft/craft.js
+++ craft/craft.js
@@ -293,8 +293,11 @@ let craft = function(target, mediaElem, zone, options) {
 		Object.keys(handles).forEach(function(key) {
 			let elem = document.createElement('div')
 			elem.className = 'handle ' + ((key || '').trim() + ' ' + (handles[key] || '').trim()).trim()
-			//elem.textContent="&nbsp;";
-			target.appendChild(elem) //.insertBefore(elem,target.firstChild)
+			if(key=='transition-indicator'){
+				target.firstChild.appendChild(elem)
+			}else{
+				target.appendChild(elem) //.insertBefore(elem,target.firstChild)
+			}
 			handles[key] = elem;
 		})
 		let savedStart=null;
