359353533bbcd77cce8eff47661f142901e3881b
ktsuttlemyre <ktsuttlemyre@users.noreply.github.com>
2023-01-20 18:15:10 -0500

    Update controlpanel.js

--- controlpanel/controlpanel.js
+++ controlpanel/controlpanel.js
@@ -1,19 +1,13 @@
-function bindControlPanel(win){
-  let window = win;
-  let document = win.document
-  let body = win.document.body
-  
-  var openFile = function(event) {
-    var input = event.target;
 
-    var reader = new FileReader();
-    reader.onload = function() {
-      var text = reader.result;
-      var node = document.getElementById('output');
-      node.innerText = text;
-      console.log(reader.result.substring(0, 200));
-    };
-    reader.readAsText(input.files[0]);
+var openFile = function(event) {
+  var input = event.target;
+
+  var reader = new FileReader();
+  reader.onload = function() {
+    var text = reader.result;
+    localStorage.setItem('KillerCommentator.config',text)
   };
+  reader.readAsText(input.files[0]);
+};
 
-}
+
