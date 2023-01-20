8d1166d46347416b6f57a150af632aeac832e9d7
ktsuttlemyre <ktsuttlemyre@users.noreply.github.com>
2023-01-20 17:59:26 -0500

    Create controlpanel.js

---
+++ controlpanel/controlpanel.js
@@ -0,0 +1,19 @@
+function bindControlPanel(win){
+  let window = win;
+  let document = win.document
+  let body = win.document.body
+  
+  var openFile = function(event) {
+    var input = event.target;
+
+    var reader = new FileReader();
+    reader.onload = function() {
+      var text = reader.result;
+      var node = document.getElementById('output');
+      node.innerText = text;
+      console.log(reader.result.substring(0, 200));
+    };
+    reader.readAsText(input.files[0]);
+  };
+
+}
