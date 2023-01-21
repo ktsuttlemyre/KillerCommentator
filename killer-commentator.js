e5f8527f088c7da8d0a0cd261ece0031639b68b7
ktsuttlemyre <ktsuttlemyre@users.noreply.github.com>
2023-01-21 16:56:30 -0500

    Update killer-commentator.js

--- killer-commentator.js
+++ killer-commentator.js
@@ -29,6 +29,11 @@ window.KillerCommentator=(function(document,KillerCommentator,pp){let inject, ap
 					window.scroll(0,0) //in case you somehow scroll away
 					main()
 				});
+				ajax(base_site+"carousel_ads.html",function(html){
+					let dom=domParse(html);
+					//prependTo(document.body,dom);
+					craft(document.createElement('div'),dom,'stage_advert')
+				});
 			});
 		})
 
