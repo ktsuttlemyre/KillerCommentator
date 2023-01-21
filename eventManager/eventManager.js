1660cd1b169da0ece08c39cb905d784f320156fa
ktsuttlemyre <ktsuttlemyre@users.noreply.github.com>
2023-01-20 21:16:57 -0500

    Create eventManager.js

---
+++ eventManager/eventManager.js
@@ -0,0 +1,4 @@
+window.eventManager=(function(document,SourceManager,pp){let inject=pp.inject, appendTo=pp.appendTo, prependTo=pp.prependTo, ajax=pp.ajax, domParse=pp.domParse;  
+
+                                                          
+ return eventManager })(document,{},plugin_platform());
