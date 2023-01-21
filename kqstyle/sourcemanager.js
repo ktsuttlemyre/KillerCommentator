b85f2f719e6d8cccd09c31d961d3e3a5b75446f9
ktsuttlemyre <ktsuttlemyre@users.noreply.github.com>
2023-01-20 22:07:53 -0500

    Update sourcemanager.js

--- kqstyle/sourcemanager.js
+++ kqstyle/sourcemanager.js
@@ -766,24 +766,46 @@ window.SourceManager=(function(document,SourceManager,pp){let inject=pp.inject, 
 		}
 	}
 							  
-							  
-							  
+const berriesPerMap={
+	day:66,
+	night:54,
+	dusk:66,
+	twilight:60,
+}
+let berries = 0
+let famineTimer;
+let famineEnd = function(){
+	
+}
 window.onGameEvent=function(event){
   let values = event.values
   switch(event.event_type){
     case gamestart:
-      const [map,goldOnLeft,elapsedTime,attractMode,version] = values
-          SourceManager.setLayout('home')
-          SourceManager.setAugment(map.replace('map_',''))
+        let [map,goldOnLeft,elapsedTime,attractMode,version] = values
+        map.replace('map_','')
+	SourceManager.setLayout('home')
+	SourceManager.setAugment(map)
+	berries = berriesPerMap[]
       break;
             
-      //case: gameend:
-      case: victory:
+    //case: gameend:
+    case victory:
       const [victor,type] = values
         SourceManager.setLayout(victor.toLowerCase())
-	SourceManager.setAugment()    
-      
-      
+	SourceManager.setAugment()
+	clearTimeout(famineTimer)
+
+    case berryDeposit: 
+      if(--berries=<0){
+	//famine start
+	famineTimer=setTimeout(famineEnd,90000)
+      }
+    break
+    case berryKickIn:
+      if(--berries=<0){
+	//famine start
+	famineTimer=setTimeout(famineEnd,90000)
+      }
       break
       default:
         console.debug('unhandled event'event)
