b1d998dc71de05ef80489ac97fb1038bd54fdc67
ktsuttlemyre <ktsuttlemyre@users.noreply.github.com>
2023-01-23 18:03:06 -0500

    Update craft.js

--- craft/craft.js
+++ craft/craft.js
@@ -668,7 +668,7 @@ let craft = function(target, mediaElem, zone, options) {
 			})
 		}
 		let associate = function(zoneInstance,dontResize) {
-			zoneInstance = craftZone.instances[ (zoneInstance?zoneInstance.id:zoneInstance) ] || zoneInstance || undefined
+			zoneInstance = craftZone.instances[ (zoneInstance?zoneInstance.id:zoneInstance) ] || craftZone.instances[zoneInstance] || zoneInstance || undefined
 			
 			//if theres something in the zone free it
 			let assCraft = zoneInstance && zoneInstance.assCraft
