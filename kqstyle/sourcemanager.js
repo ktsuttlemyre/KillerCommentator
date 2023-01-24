364bad1a3eacf32067f7d2276f58e49e2171889c
ktsuttlemyre <ktsuttlemyre@users.noreply.github.com>
2023-01-24 01:21:29 -0500

    Update sourcemanager.js

--- kqstyle/sourcemanager.js
+++ kqstyle/sourcemanager.js
@@ -742,7 +742,7 @@ let placeholders=[
 	//add bootstrap
 	appendTo('head',inject('meta',{name:"viewport", content:"width=device-width, initial-scale=1"}));
 	appendTo('head',inject('link',{href:"https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css", rel:"stylesheet", integrity:"sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD", crossorigin:"anonymous"}))
-	appendTo(document.body,inject('script',{src:"https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.min.js", crossorigin:"anonymous"},function(){
+	appendTo(document.body,inject('script',{src:"https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js", integrity:"sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM", crossorigin:"anonymous"},function(){
 		  // remote script has loaded
 	      }));
 	ajax(KillerCommentator.base_site+"templates/video_source_modal.tmpl",function(data){
