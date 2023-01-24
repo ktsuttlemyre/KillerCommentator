6c452489831fba26c0b4c4f2fa524452d7b778de
ktsuttlemyre <ktsuttlemyre@users.noreply.github.com>
2023-01-24 18:08:08 -0500

    Update sourcemanager.js

--- kqstyle/sourcemanager.js
+++ kqstyle/sourcemanager.js
@@ -786,7 +786,7 @@ function getPlaceholder(){
 				let dom=domParse(html);
 				//prependTo(document.body,dom);
 				craft(document.createElement('div'),dom,'stage_advert');
-				const carousel = new bootstrap.Carousel('#Carousel_partners')
+				const carousel = new bootstrap.Carousel('#carousel_partners')
 			});
 
 		}));
