2f61f0c8b76b8ab9223f1b1531c6374e57126232
ktsuttlemyre <ktsuttlemyre@users.noreply.github.com>
2023-01-24 17:53:23 -0500

    Update sourcemanager.js

--- kqstyle/sourcemanager.js
+++ kqstyle/sourcemanager.js
@@ -36,6 +36,7 @@ function shuffleArray(array) {
         const j = Math.floor(Math.random() * (i + 1));
         [array[i], array[j]] = [array[j], array[i]];
     }
+    return array
 }
 let placeholderPool=[];
 function getPlaceholder(){
