ef09dd0aac75d86cd5b6b81430da1525ca283c5e
ktsuttlemyre <ktsuttlemyre@users.noreply.github.com>
2023-01-25 10:44:09 -0500

    Update sourcemanager.js

--- kqstyle/sourcemanager.js
+++ kqstyle/sourcemanager.js
@@ -1,5 +1,5 @@
 window.SourceManager=(function(document,SourceManager,pp){let inject=pp.inject, appendTo=pp.appendTo, prependTo=pp.prependTo, ajax=pp.ajax, domParse=pp.domParse;
-	let config = JSON.parse(localStorage.getItem('KillerCommentator.config') || showOpenFilePicker() || `{
+	let config=SourceManager.config= JSON.parse(localStorage.getItem('KillerCommentator.config') || showOpenFilePicker() || `{
 		"urls":{
 			"challonge":[]
 		},
