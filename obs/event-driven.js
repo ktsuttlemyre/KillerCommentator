e3069db5c8d32d26a6c73f375f70c88045907ac6
ktsuttlemyre <ktsuttlemyre@users.noreply.github.com>
2024-02-05 19:12:47 -0500

    Update event-driven.js

--- obs/event-driven.js
+++ obs/event-driven.js
@@ -6,6 +6,7 @@ const berriesPerMap={
 	dusk:66,
 	twilight:60,
 }
+let end_win_screen=0
 let berries = 0
 let famineTimer;
 let famineEnd = function(){
@@ -15,23 +16,27 @@ let famineEnd = function(){
 //https://kqhivemind.com/wiki/Stats_Socket_Events
 _onGameEvent=window.onGameEvent
 
+
 window.onGameEvent=function(event){
   _onGameEvent.call(window,event)
   console.log('handling event',event)
   
   let values = event.values
-  switch(event.type){
+  switch(event.event_type){
     case 'gamestart':
         let [map,goldOnLeft,elapsedTime,attractMode,version] = values
         map.replace('map_','')
 	obsstudio.setCurrentScene('KQSFL')
 	berries = berriesPerMap[map]
+	clearTimeout(end_win_screen)
+	clearTimeout(famineTimer)
       break;
             
     //case: gameend:
-    case 'victory':
-      const [victor,type] = values
-        obsstudio.setCurrentScene('KQSFL 2')
+    case 'gameend':
+        obsstudio.setCurrentScene(event.winning_team+' wins')
+	end_win_screen=setTimeout(function(){obsstudio.setCurrentScene('KQSFL')
+},5000)
 	clearTimeout(famineTimer)
 
     case 'berryDeposit':
